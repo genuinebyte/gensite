7cd73a4a36ea4542c3f1d098ecb9a7c9501a4536
Brad Alfirevic <brad@genbyte.dev>
2020-04-10 16:09:52 -0500

    Remove broken direction,  minesweeper graticules

--- geohash/geohash.js
+++ geohash/geohash.js
@@ -1,41 +1,52 @@
+/** Class for calculating a hash point */
 class GeoHash {
-	constructor() {
-		this.date = new Date().toISOString().split('T')[0];
+	constructor(date, dow) {
+		this.setDate(date);
+		this.setDow(dow);
+		this.makeFractions();
 	}
 
-	calcTarget() {
+	setDate(date) {
+		if (!date) {
+			this.date = new Date().toISOString().split('T')[0];
+		} else {
+			this.date = date;
+		}
+
+		this.dirty = true;
+	}
+
+	setDow(dow) {
+		if (!dow) {
+			//TODO: Attempt to retreive dow
+		} else {
+			this.dow = dow;
+		}
+
+		this.dirty = true;
+	}
+
+	makeFractions() {
 		let hash = MD5(this.getHashingString());
 		let firstHalf = hash.substr(0,16);
 		let secondHalf = hash.substr(16);
 
-		this.graticulLatitude = Math.trunc(this.latitude);
-		this.graticulLongitude = Math.trunc(this.longitude);
+		let firstFraction = this.hexToFrac(firstHalf);
+		let secondFraction = this.hexToFrac(secondHalf);
 
-		this.firstFraction = this.hexToFrac(firstHalf);
-		this.secondFraction = this.hexToFrac(secondHalf);
-
-		this.targetLatitude = truncateDecimals(
-			this.graticulLatitude + (this.graticulLatitude > 0 ? this.firstFraction : this.firstFraction * -1), 5);
-		this.targetLongitude = truncateDecimals(
-			this.graticulLongitude + (this.graticulLongitude > 0 ? this.secondFraction : this.secondFraction * -1), 5);
-
-		return true;
+		this.dirty = false;
+		this.fractions = new Vec2(firstFraction, secondFraction);
 	}
 
-	currentLocation(success, error) {
-		this.successCallback = success;
-
-		function internal_success(position) {
-			this.latitude = position.coords.latitude;
-			this.longitude = position.coords.longitude;
-			this.successCallback();
+	getTarget(lat, lon) {
+		if (this.dirty) {
+			this.makeFractions();
 		}
 
-		if (!navigator.geolocation) {
-			return false;
-		} else {
-			navigator.geolocation.getCurrentPosition(internal_success.bind(this), error);
-		}
+		return new Vec2(
+			Math.absAdd(Math.trunc(lat), this.fractions.x),
+			Math.absAdd(Math.trunc(lon), this.fractions.y)
+		);
 	}
 
 	getHashingString() {
@@ -47,7 +58,8 @@ class GeoHash {
 		return "http://geo.crox.net/djia/" + parts[0] + "/" + parts[1] + "/" + parts[2];
 	}
 
-	// Does not deal with https://geohashing.site/geohashing/30W_Time_Zone_Rule yet
+	// Does not deal with https://geohashing.site/geohashing/30W_Time_Zone_Rule
+	// TODO: Use geo.crox.net through HTTPS relay at geo.genbyte.dev
 	getCurrentDow(load) {
 		this.dowLoadCallback = load;
 		function internal_load(resp) {
@@ -72,78 +84,82 @@ class GeoHash {
 	}
 }
 
-class LocationWatcher {
-	constructor(success, error, tick, isHighAccuracy) {
-		this.successCallback = success;
-		this.errorCallback = error;
-		this.tickCallback = tick;
-		this.isHighAccuracy = isHighAccuracy;
-		this.headings = [];
+class LocationProvider {
+	constructor() {
+		this.setHighAccuracy(false);
+		this.setTimeout(1000);
 	}
 
-	watch() {
-		function success(pos) {
-			this.latitude = pos.coords.latitude;
-			this.longitude = pos.coords.longitude;
-			let heading = pos.coords.heading;
+	setSuccessCallback(fn) {
+		this.successCallback = fn;
+	}
 
-			this.headings.push(heading);
-			if (!isNaN(heading) && this.headings.length > 4) {
-				this.headings.splice(0, 1);
-			}
+	setErrorCallback(fn) {
+		this.errorCallback = fn;
+	}
 
-			this.age = 0;
-			this.successCallback(this);
+	setTickCallback(fn) {
+		this.tickCallback = fn;
+	}
+
+	setHighAccuracy(tf) {
+		this.isHighAccuracy = tf;
+	}
+
+	setTimeout(num) {
+		this.timeout = num;
+	}
+
+	internalSuccess(pos) {
+		this.location = new Vec2(
+			pos.coords.latitude,
+			pos.coords.longitude
+		);
+		this.age = 0;
+		this.successCallback(this);
+		this.tickCallback(this);
+	}
+
+	internalError(err) {
+		if (err.code == err.TIMEOUT) {
+			this.age += this.timeout/1000;
 			this.tickCallback(this);
+		} else {
+			this.errorCallback(this);
+		}
+	}
+
+	get() {
+		if (this.watcher) {
+			return false;
 		}
 
-		function error(err) {
-			if (err.code == err.TIMEOUT) {
-				this.age += 0.25;
-				this.tickCallback(this);
-			} else {
-				this.errorCallback(this);
-			}
-		}
-
-		this.watcher = navigator.geolocation.watchPosition(
-			success.bind(this),
-			error.bind(this),
-			{"enableHighAccuracy": this.isHighAccuracy, "timeout": 250}
+		navigator.geolocation.getCurrentPosition(
+			this.internalSuccess.bind(this),
+			this.internalError.bind(this),
+			{"enableHighAccuracy": this.isHighAccuracy, "timeout": this.timeout}
 		);
 	}
 
-	getHeading() {
-		let sum = 0;
-		for (let i = 0; i < this.headings.length; ++i) {
-			sum += this.headings[i];
-		}
-
-		return sum/this.headings.length;
+	watch() {
+		this.watcher = navigator.geolocation.watchPosition(
+			this.internalSuccess.bind(this),
+			this.internalError.bind(this),
+			{"enableHighAccuracy": this.isHighAccuracy, "timeout": this.timeout}
+		);
 	}
 
 	stop() {
-		this.latitude = 0;
-		this.longitude = 0;
 		this.age = -1;
 		navigator.geolocation.clearWatch(this.watcher);
 	}
 
-	distanceKm(lat, lon) {
-		return this.getDistanceFromLatLonInKm(this.latitude, this.longitude, lat, lon);
+	distanceKm(vec) {
+		return this.getDistanceFromLatLonInKm(this.location.x, this.location.y, vec.x, vec.y);
 	}
 
-	distanceMi(lat, lon) {
-		return  truncateDecimals(this.distanceKm(lat, lon) * 0.6213712, 2);
-	}
-
-	angle(lat, lon) {
-		let dot = (lat*this.latitude) + (lon*this.longitude);
-			console.log(dot);
-		let mags = Math.sqrt(lat*lat + lon*lon) * Math.sqrt(this.latitude*this.latitude + this.longitude*this.longitude);
-			console.log(mags);
-
-		return Math.acos(dot/mags);
+	distanceMi(vec) {
+		return this.distanceKm(vec) * 0.6213712;
 	}
 
 	// https://stackoverflow.com/a/27943/10354782
@@ -162,12 +178,12 @@ class LocationWatcher {
 	}
 
 	deg2rad(deg) {
-		return deg * (Math.PI/180)
+		return deg * (Math.PI/180);
 	}
 }
 
 // https://stackoverflow.com/a/9232092/10354782
-function truncateDecimals(num, digits) {
+Math.truncateDecimals = function(num, digits) {
 	var numS = num.toString(),
 		decPos = numS.indexOf('.'),
 		substrLength = decPos == -1 ? numS.length : 1 + decPos + digits,
@@ -177,78 +193,183 @@ function truncateDecimals(num, digits) {
 	return parseFloat(finalResult);
 }
 
-window.onload = function winload() {
-	window.geohash = new GeoHash();
-	document.getElementById("date").value = geohash.date;
-
-	let dowUrl = geohash.getDowUrl();
-	document.getElementById("dowlink").href = dowUrl;
-
-	window.geodom = {
-		'lat': document.getElementById('lat'),
-		'long': document.getElementById('long'),
-		'date': document.getElementById('date'),
-		'dow': document.getElementById('dow'),
-		'tlat': document.getElementById('tlat'),
-		'tlong': document.getElementById('tlong'),
-		'nlat': document.getElementById('nlat'),
-		'nlong': document.getElementById('nlong')
-	};
+Math.absAdd = function(whole, frac) {
+	return (Math.abs(whole) + Math.abs(frac)) * Math.sign(whole) * Math.sign(frac);
 }
 
-function calcHash() {
-	geohash.latitude = geodom.lat.value;
-	geohash.longitude = geodom.long.value;
-	geohash.date = geodom.date.value;
-	geohash.dow = geodom.dow.value;
+class Vec2 {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
 
-	if (window.geohash.calcTarget()) {
-		document.getElementById('tlat').innerHTML = geohash.targetLatitude;
-		document.getElementById('tlong').innerHTML = geohash.targetLongitude;
-	} else {
-		alert("Failed to calculate target coordinates!");
+	truncateDecimals(n) {
+		return new Vec2(
+			Math.truncateDecimals(this.x, n),
+			Math.truncateDecimals(this.y, n)
+		);
+	}
+
+	add(x, y) {
+		return new Vec2(
+			this.x+x,
+			this.y+y
+		);
 	}
 }
 
-function getLocation() {
-	function success() {
-		document.getElementById("lat").value = geohash.latitude;
-		document.getElementById("long").value = geohash.longitude;
+class GeoUI {
+	constructor() {
+		window.onload = this.winload.bind(this);
 	}
 
-	function error() {
-		alert("Unable to get your location.");
+	winload() {
+		this.dom = {
+			'lat': document.getElementById('lat'),
+			'lon': document.getElementById('long'),
+			'date': document.getElementById('date'),
+			'dow': document.getElementById('dow'),
+			'dowlink': document.getElementById('dowlink'),
+			'tlat': document.getElementById('tlat'),
+			'tlon': document.getElementById('tlong'),
+			'nlat': document.getElementById('nlat'),
+			'nlon': document.getElementById('nlong'),
+			'age': document.getElementById('locage'),
+			'crow': document.getElementById('crow'),
+			'crowN': document.getElementById('crowN'),
+			'crowNE': document.getElementById('crowNE'),
+			'crowE': document.getElementById('crowE'),
+			'crowSE': document.getElementById('crowSE'),
+			'crowS': document.getElementById('crowS'),
+			'crowSW': document.getElementById('crowSW'),
+			'crowW': document.getElementById('crowW'),
+			'crowNW': document.getElementById('crowNW'),
+		};
+
+		this.geohash = new GeoHash();
+		this.lprov = new LocationProvider();
+		this.lprov.setTimeout(250);
+
+		this.dom.date.value = this.geohash.date;
+		//TODO: Temporary fix until I get my HTTPS relay setup
+		this.dom.dowlink.href = this.geohash.getDowUrl();
 	}
 
-	geohash.currentLocation(success, error);
-}
+	getCurrentLocation() {
+		function success(lprov) {
+			this.setCurrent(lprov.location);
+			this.putCurrent();
+			this.displayCurrent();
+		}
 
-function watchPos(isAccurate) {
-	function success(lwatcher) {
-		document.getElementById("nlat").innerHTML = truncateDecimals(lwatcher.latitude, 5);
-		document.getElementById("nlong").innerHTML = truncateDecimals(lwatcher.longitude, 5);
+		function error() {
+			alert("Failed to get location");
+		}
 
-		if (geohash.targetLatitude != 0) {
-			document.getElementById("crowflies").innerHTML =
-				lwatcher.distanceMi(geohash.targetLatitude, geohash.targetLongitude);
+		this.lprov.setSuccessCallback(success.bind(this));
+		this.lprov.setErrorCallback(error.bind(this));
+		this.lprov.get();
+	}
 
-			let x = geohash.targetLatitude - lwatcher.latitude;
-			let y = geohash.targetLongitude - lwatcher.longitude;
-			let angle = ((Math.atan(x/y) * (180/Math.PI))) + (90 + lwatcher.getHeading());
-			document.getElementById("angle").innerHTML = "x: " + x + "y: " + y + " | " + angle + " [" + lwatcher.getHeading() + "]";
-			document.getElementById("direction").style.transform = "rotate(" + (angle) + "deg)";
+	watchLocation(highAccuracy) {
+		function success(lprov) {
+			this.setCurrent(lprov.location);
+			this.displayCurrent();
+			this.displayCrow();
+		}
+
+		function error() {
+			alert("Failed to get location");
+		}
+
+		function tick(lprov) {
+			this.dom.age.innerHTML = lprov.age + "s";
+		}
+
+		this.lprov.setSuccessCallback(success.bind(this));
+		this.lprov.setErrorCallback(error.bind(this));
+		this.lprov.setTickCallback(tick.bind(this));
+		this.lprov.setHighAccuracy(highAccuracy);
+		this.lprov.watch();
+	}
+
+	calculateHash() {
+		console.log(this);
+		let lat = this.dom.lat.value;
+		let lon = this.dom.lon.value;
+		let date = this.dom.date.value;
+		let dow = this.dom.dow.value;
+
+		if (date && dow) {
+			this.geohash.setDate(date);
+			this.geohash.setDow(dow);
+		} else if (date && !dow) {
+			return;
+			this.geohash.setDate(date);
+			this.geohash.setDow();
+		} else {
+			return;
+			this.geohash.setDate();
+			this.geohash.setDow();
+		}
+		this.setTarget(this.geohash.getTarget(lat, lon));
+
+		this.displayTarget();
+		this.displayCrow();
+	}
+
+	prettyDistance(target) {
+		let dist = this.lprov.distanceKm(target);
+
+		if (dist >= 100) {
+			return Math.trunc(dist) + "km";
+		} else if (dist >= 10) {
+			return Math.truncateDecimals(dist, 1) + "km";
+		} else if (dist >= 1) {
+			return Math.truncateDecimals(dist, 2) + "km";
+		} else {
+			// Meters
+			return Math.truncateDecimals(dist*1000, 2) + "m"
 		}
 	}
 
-	function error(lwatcher) {
-		alert("Failed to get your location");
+	setCurrent(vec) {
+		this.current = vec.truncateDecimals(5);
 	}
 
-	function tick(lwatcher) {
-		let age = lwatcher.age;
-		document.getElementById("locage").innerHTML = age == 0 ? "now" : age + " seconds";
+	setTarget(vec) {
+		this.target = vec.truncateDecimals(5);
 	}
 
-	window.lwatcher = new LocationWatcher(success, error, tick, isAccurate);
-	lwatcher.watch();
+	putCurrent() {
+		this.dom.lat.value = this.current.x;
+		this.dom.lon.value = this.current.y;
+	}
+
+	displayTarget() {
+		this.dom.tlat.innerHTML = this.target.x;
+		this.dom.tlon.innerHTML = this.target.y;
+	}
+
+	displayCurrent() {
+		this.dom.nlat.innerHTML = this.current.x;
+		this.dom.nlon.innerHTML = this.current.y;
+	}
+
+	displayCrow() {
+		if (this.current || this.target) {
+			let distance = this.prettyDistance.bind(this);
+			this.dom.crow.innerHTML = distance(this.target);
+			this.dom.crowN.innerHTML = distance(this.target.add(1, 0));
+			this.dom.crowNE.innerHTML = distance(this.target.add(1, 1));
+			this.dom.crowE.innerHTML = distance(this.target.add(0, 1));
+			this.dom.crowSE.innerHTML = distance(this.target.add(-1, 1));
+			this.dom.crowS.innerHTML = distance(this.target.add(-1, 0));
+			this.dom.crowSW.innerHTML = distance(this.target.add(-1, -1));
+			this.dom.crowW.innerHTML = distance(this.target.add(0, -1));
+			this.dom.crowNW.innerHTML = distance(this.target.add(1, -1));
+		}
+	}
 }
+
+window.geoui = new GeoUI();
